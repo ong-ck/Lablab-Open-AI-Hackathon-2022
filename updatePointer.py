3dccfe5454e12e04fe1f486f54ca37a8da08f9a1
ongck0808 <85627680+ong-ck@users.noreply.github.com>
2022-12-16 19:39:54 +0000

    update file reference

--- updatePointer.py
+++ updatePointer.py
@@ -1,11 +1,11 @@
 def updatePointer():
-  with open("/home/runner/Streamlit-Test/pointer.txt") as fin:
+  with open("/home/runner/Lablab-Open-AI-Hackathon-2022/pointer.txt") as fin:
     lines = fin.read()
   newLine = (int(lines) + 1) % 9
-  with open("/home/runner/Streamlit-Test/pointer.txt", 'r+') as fin:
+  with open("/home/runner/Lablab-Open-AI-Hackathon-2022/pointer.txt", 'r+') as fin:
       fin.write(str(newLine))
 
 def getPointer():
-  with open("/home/runner/Streamlit-Test/pointer.txt") as fin:
+  with open("/home/runner/Lablab-Open-AI-Hackathon-2022/pointer.txt") as fin:
     lines = fin.read()
   return int(lines)
--- updateScore.py
+++ updateScore.py
@@ -1,20 +1,20 @@
 def updateScore():
-  with open("/home/runner/Streamlit-Test/score.txt") as fin:
+  with open("/home/runner/Lablab-Open-AI-Hackathon-2022/score.txt") as fin:
     lines = fin.read()
   newLine = int(lines) + 1
-  with open("/home/runner/Streamlit-Test/score.txt", 'r+') as fin:
+  with open("/home/runner/Lablab-Open-AI-Hackathon-2022/score.txt", 'r+') as fin:
       fin.write(str(newLine))
 
 def printScore():
-  with open("/home/runner/Streamlit-Test/score.txt") as fin:
+  with open("/home/runner/Lablab-Open-AI-Hackathon-2022/score.txt") as fin:
       lines = fin.read()
   print("Score: ", lines)
   return lines
 
 def resetScore():
-  with open("/home/runner/Streamlit-Test/score.txt", 'r+') as fin:
+  with open("/home/runner/Lablab-Open-AI-Hackathon-2022/score.txt", 'r+') as fin:
       fin.truncate(0)
       fin.write("0")
-  with open("/home/runner/Streamlit-Test/score.txt") as fin:
+  with open("/home/runner/Lablab-Open-AI-Hackathon-2022/score.txt") as fin:
     lines = fin.read()
   print("Score reset to ", lines)
